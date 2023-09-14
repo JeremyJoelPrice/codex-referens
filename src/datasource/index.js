f50506e400d16afb70e855f627664509f01fd8d1
Jeremy Joel Price <JeremyJoelPrice@gmail.com>
2023-09-15 00:15:12 +0300

    amended owyn's backstory

--- src/datasource/index.js
+++ src/datasource/index.js
@@ -118,10 +118,9 @@ She prefers to go unseen and will hide in the shadows if she can.`
 		name: "Volrin Ironspring",
 		img: volrinPortrait,
 		description: ``,
-		backstory: `(Work In Progress)
-He lived in a volcanic mountain alongside other gnomes and dwarves. Within this mountain was a great source of heat (hence volcanic, but could also be magical or down to a dragon, possibilities are open, can leave thst to your discretion).
+		backstory: `Volrin lived in the volcano Archaedra alongside other gnomes and dwarves. Within this mountain was a great source of heat said to come from a mythical dragon deep below.
 
-The gnomes and drawves within the mountain worked together to forge and produce metal goods (armours, weapons, robots, steam machines etc) using the heat from the mountain. They all lived harmoniously, until one day the big bad enemy (either an army or a group led by a leader) began fracking the mountain for its minerals and rare metals that the dwarves and gnomes used to build.
+The gnomes and dwarves within the mountain worked together to forge and produce metal goods (armours, weapons, steam machines etc) using the semi-magical heat from the mountain. They all lived harmoniously, until one day a mysterious invading army began fracking the mountain for its minerals and rare metals that the dwarves and gnomes used to build.
 
 The gnomes and dwarves that were trained tried to fight, whilst the others (mine included) were forced out of the mountain and away from their homes. Many of them settled together others went seeking revenge`
 	},
