90bbf096ad0d7ba0532575d9da478f836090a5ed
Jeremy Joel Price <JeremyJoelPrice@gmail.com>
2024-03-16 01:15:37 +0000

    update character info

--- src/config/config.js
+++ src/config/config.js
@@ -3,18 +3,28 @@ import naz from "../datasource/img/naz.jpg";
 import xanthe from "../datasource/img/xanthe.jpg";
 import ghost from "../datasource/img/ghost.jpg";
 import finnian from "../datasource/img/finnian.jpg";
+import aira from "../datasource/img/aria.jpg";
+import quinn from "../datasource/img/quinn.jpg";
 
 const config = {
 	"player characters": [
 		{
 			name: "Finnian Reid",
 			img: finnian,
-			backstory: `As a child Finnian met the legendary Dr Joseph Bell, a famous investigator, renowned for his ability to solve mystery. Quickly enamoured, Finnian dedicated his life to following Dr Bells footsteps, dedicated to becoming fabled. As his lust for money and fame took over, Finnian found himself struggling to balance between the fine line of greed and morale, often taking jobs with the promise of fame and coin regardless of the impact it could cause. He practised inventing new weapons and armour in his quest for notoriety, often selling pieces to fund his projects and extravagant life style. One ominous night he received a mysterious letter from a Lord Beaumont, summoning him to his Manor for quest destined pave his future.`
+			backstory: `As a child Finnian met the legendary Dr Joseph Bell, a famous investigator, renowned for his ability to solve mystery. Quickly enamoured, Finnian dedicated his life to following Dr Bells footsteps, dedicated to becoming fabled.
+			
+			As his lust for money and fame took over, Finnian found himself struggling to balance between the fine line of greed and morale, often taking jobs with the promise of fame and coin regardless of the impact it could cause. He practised inventing new weapons and armour in his quest for notoriety, often selling pieces to fund his projects and extravagant life style.
+			
+			One ominous night he received a mysterious letter from a Lord Beaumont, summoning him to his Manor for quest destined pave his future.`
 		},
 		{
 			name: "Naz",
 			img: naz,
-			backstory: `Naz grew up in the slums of Blackchapel. She didn’t know her parents and begged for money. When she got older she had a scrap with some kids who tried to steal her pet rat, Fernando. She was spotted by another half-orc who ran a fighting ring in the sewers. He trained her as a fighter and she fought for him in his illegal fighting ring in the sewers where people come and go and make bets. She wins most of her money this way as she is very small for a half-orc so people tend to bet against her, but she rarely loses. Her mentor was a bit like a father but was distant and only saw her as a prized fighter. Although she hasn’t experience love she hopes one day she will. She works in kitchens for a stable salary since her race prevented her from education or any better job. She is wary of the other races. She dreams of escaping the slums and often looks up at the stars wishing for freedom and safety. She was working her day job back of house in the kitchen when she received a mysterious letter…`
+			backstory: `Naz grew up in the slums of Blackchapel. She didn’t know her parents and begged for money. When she got older she had a scrap with some kids who tried to steal her pet rat, Fernando.
+			
+			She was spotted by another half-orc who ran a fighting ring in the sewers. He trained her as a fighter and she fought for him in his illegal fighting ring in the sewers where people come and go and make bets. She wins most of her money this way as she is very small for a half-orc so people tend to bet against her, but she rarely loses. Her mentor was a bit like a father but was distant and only saw her as a prized fighter. Although she hasn’t experience love she hopes one day she will.
+			
+			She works in kitchens for a stable salary since her race prevented her from education or any better job. She is wary of the other races. She dreams of escaping the slums and often looks up at the stars wishing for freedom and safety. She was working her day job back of house in the kitchen when she received a mysterious letter…`
 		},
 		{
 			name: "Melody Grieves",
@@ -34,8 +44,33 @@ const config = {
 		{
 			name: "Xanthe Ravenna",
 			img: xanthe,
-			backstory: `I am Xanthe Ravenna a half-elf warlock I left my homeland with my Pseudo-dragon - Ghost in search of knowledge and I endeavoured to uncover the mysteries and secrets of the world, unfortunately in my search I stumbled upon an ancient bookcase not too long ago and read a passage out loud, this proved to be a terrible mistake! I was cursed by the devil himself and have been embroiled in my own internal battle ever since I am kind hearted and polite and inherently wish to make good decisions and the right choices but the curse is trying to drag me into darkness and will stop at nothing to corrupt my heart and soul My inner conflict is symbolised by tattoo-like marks on my body, Every time I defy the curse and make the right choice, but not necessarily the easiest choice I am made to suffer! It is as though a flaming torch has been held to my skin and when the pain subsides I am forever marked, my sincere hope is that one day I will find the cure to my affliction and once again regain control of my soul. I was stopped at a tavern for some sustenance when I was approached by a mysterious stranger, they passed me a letter and left without a word
-			This letter has led me to Bensons Solicitors, I have arrived at the specified location eager to uncover the secrets that have been laid in my path`
+			backstory: `I am Xanthe Ravenna a half-elf warlock I left my homeland with my Pseudo-dragon - Ghost in search of knowledge and I endeavoured to uncover the mysteries and secrets of the world, unfortunately in my search I stumbled upon an ancient bookcase not too long ago and read a passage out loud, this proved to be a terrible mistake! I was cursed by the devil himself and have been embroiled in my own internal battle ever since.
+			
+			I am kind hearted and polite and inherently wish to make good decisions and the right choices but the curse is trying to drag me into darkness and will stop at nothing to corrupt my heart and soul My inner conflict is symbolised by tattoo-like marks on my body. Every time I defy the curse and make the right choice, but not necessarily the easiest choice I am made to suffer! It is as though a flaming torch has been held to my skin and when the pain subsides I am forever marked, my sincere hope is that one day I will find the cure to my affliction and once again regain control of my soul.
+			
+			I was stopped at a tavern for some sustenance when I was approached by a mysterious stranger, they passed me a letter and left without a word. This letter has led me to Bensons Solicitors, I have arrived at the specified location eager to uncover the secrets that have been laid in my path`
+		},
+		{
+			name: "Aria",
+			img: aira,
+			backstory: `On the night of the first blood moon in 200 years, Aria escaped the slaughter of her lan only through luck - she was at her favourite clearing to stargaze.
+
+			As a result, she abandoned the wilds and took herself to the city. She now has an intense fear of ever returning to the wilds she once called her home.
+
+			However, in her time in the cit she is losing her connection to the stars, only catching sight of them every few weeks. This is causing her to lose her grip on reality and seek the stars wherever she can.`
+		},
+		{
+			name: "Quinn",
+			img: quinn,
+			backstory: `Quinn’s parents were too poor to care for her and she barely remembers them. When she thinks of them she thinks of wild woods and crackling fires. She was left as a toddler at a village church. Along with a note that simply said “We love you Quinn, we know you might not understand why we couldn’t keep you but you’ll always be our daughter. Remember that.”.
+
+		Quinn spent a few years at the village’s orphanage before she was adopted age 6 by a local man grieving the loss of his wife and son who tragically drowned in a nearby river years prior. Viktor was her father and mentor and he trained her to be strong, fast and patient. He was a human ranger and taught her how to hunt, take care of herself and even taught her how to cast spells using nature’s power. He taught her how to defend herself from other humans and monsters alike.
+		
+		One night, when Quinn was 16, the village was under attack. A monster, Viktor had said. He sent her away to the woods to hide. She hid for hours before she decided she wanted to fight. When she returned to the village the door to her home was in pieces. Viktor was gone and nothing but blood and shreds of his clothing remained. Quinn ran. She ran to the woods and spent 2 years living alone with nothing but grief and regret. Wanting her father back so badly she reached out to the woods asking for companionship and bonded with a nearby bee hive. They became her family. Eventually she found her way to the city of Blackchapel. Quinn performed on the street for money.
+		
+		She did acts for pedestrians using her bees. One day a travelling circus passed by and she was spotted by a performer. They offered her a job on the spot. Quinn took it without a second thought. A family, security and a place her and her bees could call home. She is now 24 years old and her fellow performers are her family. She often wonders about her biological parents and grieves the loss of her adopted father. She hates monsters and sometimes wonders the streets hoping to find one preying on an innocent.
+		
+		Her morality is neutral, she often acts for good but she follows no god and can be driven by grief and vengeance. She’s known enough pain and loss to have never taken a lover. Her only friends are those in the circus. She’s not exactly approachable or friendly to anyone else but she has a soft spot for lost souls (like hers) and she can sniff them out very easily. Bees?`
 		},
 		{
 			name: "Ghost",
