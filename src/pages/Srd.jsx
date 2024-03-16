b83d76925597e851b1125afcda43e3bdea55387c
Jeremy Joel Price <JeremyJoelPrice@gmail.com>
2024-03-16 01:05:07 +0000

    update srd

--- src/pages/Srd.jsx
+++ src/pages/Srd.jsx
@@ -5,72 +5,37 @@ import {
 	Header,
 	Subheader
 } from "../components/StyledArticleComponents";
-import wallpaper from "../datasource/img/wallpaper.jpg"
+import wallpaper from "../datasource/img/wallpaper.jpg";
 
 const Srd = () => {
 	return (
 		<Wallpaper src={wallpaper}>
 			<StyledSrd>
 				<ContentColumn>
-					<Header>House Rules & Etiquette</Header>
-					<Subheader>0. Be lush unto one another</Subheader>
-					<BodyText>So continue as you are</BodyText>
-					<Subheader>1. DM Rulings Are Final</Subheader>
+					<CenteredText>
+						<CenteredSubheader>
+							House Rules & Etiquette
+						</CenteredSubheader>
+						<BodyText>
+							0. Be lush unto one another
+							<br />
+							1. DM Rulings Are Final
+							<br />
+							2. No Retconning
+							<br />
+							3. No PvP Without Consent
+							<br />
+						</BodyText>
+					</CenteredText>
+					<CenteredSubheader>"Tactical" Playstyle</CenteredSubheader>
 					<BodyText>
-						The official rules are second to the DM's rulings; a
-						useful tool but time-consuming to constantly reference
-						during play. The DM will look up the official rule after
-						the game if there was any doubt, but is never bound by
-						what's in the book.
-					</BodyText>
-					<Subheader>2. No Retconning</Subheader>
-					<BodyText>
-						If a rule was mis-remembered, and just in general, we
-						typically won't undo what's happened in game. Suspension
-						of disbelief is almost always more important
-					</BodyText>
-					<Subheader>3. No PvP Without Consent</Subheader>
-					<BodyText>
-						If your character wants to take actions which harm
-						another PC, we don't engage the rules. Instead, both
-						players come to an agreement about what happens next. If
-						you can't agree, then nothing happens.
-					</BodyText>
-					<Header>Playstyle & Tone As Per Session 0</Header>
-					<BodyText>
-						We have settled on a game world that is open, where
-						there is more adventure and opportunity to be found than
-						you will ever be able to complete, and players are
-						expected to be self-motivated, and push forward in
-						pursuit of their own goals. This world is also filled
-						with many factions, who are looking for
-						mercenaries/adventurers to hire. The world is also
-						gritty, and dangerous. Encounters are not designed to be
-						fair, only to be plausible within the game world.
-						Characters who rush into combat without preparing or
-						researching their opponents will likely get into fights
-						they can't win.
-					</BodyText>
-					<br />
-					<BodyText>
-						Secondary to the above, this is a game featuring some
-						intrigue, an overarching plot that can't be ignored
-						indefinitely, and plenty of breathing room for
-						slice-of-life moments between the action and drama.
-					</BodyText>
-					<br />
-					<BodyText>
-						Remember:
 						<ul>
 							<li>
-								your character sheet doesn't contain the
-								solution to every problem
+								The enemies want to win, and will act in their
+								own self interest
 							</li>
 							<li>
-								your enemies think they're the main characters
-							</li>
-							<li>
-								no amount of hit points can save you from a
+								No amount of hit points can save you from a
 								logical death (like a knife in the neck, or a
 								fall from a great height).
 							</li>
@@ -80,23 +45,13 @@ const Srd = () => {
 					<BodyText>
 						<ul>
 							<li>
-								Base XP earned by attending a session, assuming
-								something dramatic and dangerous happens.
+								Consistently choosing the quick and easy "evil"
+								option will cause alignment to downshift
 							</li>
 							<li>
-								Bonus XP is earned by discovering hidden areas,
-								and discovering NPC aspects (their flaws, bonds,
-								etc.)
-							</li>
-							<li>
-								Gritty long/short rest variant However, spell
-								casters can prepare their spells after a short
-								rest instead of requiring a long rest.
-							</li>
-							<li>
-								Inspiration is awarded for representing your
-								personality traits, flaws etc in a way which is
-								detrimental and dramatic
+								Inspiration is also awarded for doing the
+								"morally right" thing when it's inconvenient or
+								problematic to do so
 							</li>
 						</ul>
 					</BodyText>
@@ -124,4 +79,12 @@ const ContentColumn = styled.div`
 	@media (max-width: 750px) {
 		padding: 0;
 	}
+`;
+
+const CenteredText = styled.div`
+	text-align: center;
+`;
+
+const CenteredSubheader = styled(Subheader)`
+	text-align: center;
 `;
