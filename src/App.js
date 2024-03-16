469b2cb385e1ee1ebde8db66f6e1663b440f2c9f
Jeremy Joel Price <JeremyJoelPrice@gmail.com>
2024-03-16 00:10:57 +0000

    remove redundant import

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import CharacterGallery from "./pages/CharacterGallery";
 import Navbar from "./components/Navbar";
 import Srd from "./pages/Srd";
 import { useState } from "react";
-import config from "./config/config";
 
 const App = () => {
 	const [displayCharacter, setDisplayCharacter] = useState();
